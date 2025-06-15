d2fe31c820e84ae5e20c17f2e1d7df6bf5a485bb
Mister-Mario <mariojr2003@gmail.com>
2025-06-15 08:52:39 +0200

    Fallback for help guides

--- Clipboard++/Utils/LinkManager.cpp
+++ Clipboard++/Utils/LinkManager.cpp
@@ -8,7 +8,11 @@
 void openHelp()  {
     QLocale default_locale = QLocale::system();
     QString locale_name = default_locale.name();
-    QString link_string = QString("https://github.com/Mister-Mario/Advanced-Clipboard/blob/main/help-guides/help-%1.md").arg(locale_name);
+    QString link_string = "";
+    if(locale_name.contains("es"))
+        link_string = "https://github.com/Mister-Mario/Advanced-Clipboard/blob/main/help-guides/help-es_ES.md";
+    else
+        link_string = "https://github.com/Mister-Mario/Advanced-Clipboard/blob/main/help-guides/help-en_US.md";
     QUrl url(link_string);
     if (!QDesktopServices::openUrl(url)) {
         Rml::Log::Message(Rml::Log::LT_ERROR, "Can't open link '%s'", link_string);
